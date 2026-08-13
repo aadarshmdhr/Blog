3a08a4a27017c1a132817790f12a49c817bbbc37
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2026-08-13 17:16:12 +0545

    Added draft list and details imported login required inviews.py

--- blog_app/views.py
+++ blog_app/views.py
@@ -14,15 +14,40 @@ def post_list(request):
         {"posts": posts},
     )
 
+
 def post_detail(request, pk):
-    post= Post.objects.get(pk=pk)
+    post = Post.objects.get(pk=pk)
     return render(
         request,
         "post_detail.html",
         {"post": post},
     )
 
+
 def post_delete(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
     return HttpResponseRedirect("/")
+
+
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def draft_list(request):
+    post = Post.objects.filter(published_date__isnull=True)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": post},
+    )
+
+
+@login_required
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_date__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
+    )
